a27769ab6615f027a404963d7181d98424c76f4f
Karen Cespedes Rivas <cesp.karen@gmail.com>
2024-08-19 15:47:14 -0300

    Creacion de rama Javascript y 1er ejercicio

---
+++ Javascript/Clase 1/Ejercicio - anio Bisiesto.js
@@ -0,0 +1,16 @@
+//Ejercicio Anio Bisiesto
+
+
+let option = 0;
+console.log(" Comprobamos que el anio es Bisiesto");
+while (option==0) {
+    let anio = parseInt(input("Ingrese el anio"));
+
+if((num%4 == 0 && num % 100 != 0) || num% 4 == 0){
+ console.log("El anio es bisiesto"+ anio)
+}else{
+    console.log("El anio no es bisiesto" + anio);
+}
+opcion = parseInt(input("Para salir ingrese 1"));
+}
+
